892e6735caa034db4cb0c2dc7026b7b27318f545
naseh42 <naseh42@gmail.com>
2025-03-22 03:05:55 +0330

    Update __init__.py

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,5 +1,6 @@
 from .users import router as users_router
 from .domains import router as domains_router
 from .settings import router as settings_router
+from .subscription import router as subscription_router
 
-__all__ = ["users_router", "domains_router", "settings_router"]
+__all__ = ["users_router", "domains_router", "settings_router", "subscription_router"]
