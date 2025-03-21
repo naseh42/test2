cdc7a0148c817fcdb32006fdc847b6d5aa15e8f7
naseh42 <naseh42@gmail.com>
2025-03-21 21:02:35 +0330

    Create users.py

---
+++ backend/routers/users.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from backend.models.models import User
+from backend.database.database import get_db
+
+router = APIRouter()
+
+# دریافت لیست کاربران
+@router.get("/")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
+
+# دریافت جزئیات یک کاربر
+@router.get("/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+# ایجاد یک کاربر جدید
+@router.post("/")
+def create_user(user: User, db: Session = Depends(get_db)):
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+# به‌روزرسانی اطلاعات کاربر
+@router.put("/{user_id}")
+def update_user(user_id: int, updated_user: User, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    for key, value in updated_user.dict().items():
+        setattr(user, key, value)
+    db.commit()
+    return user
+
+# حذف کاربر
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(user)
+    db.commit()
+    return {"message": "User deleted successfully"}
